433ae6ed418916897cac31f4c06f2d3e5e660e1e
KyoungA Kang <kkang@gradcenter.cuny.edu>
2022-03-20 00:04:31 -0400

    added sequential color scale and legend

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+const width = window.innerWidth * 0.8,
+      height = window.innerHeight * 0.8,
+      margin = { top: 20, bottom: 50, left: 40, right: 40 };      
 
 /**
  * LOAD DATA
@@ -9,18 +9,120 @@
  * */
 Promise.all([
   d3.json("../data/usState.json"),
-  d3.csv("../data/stateCapitals.csv", d3.autoType),
-]).then(([geojson, capitals]) => {
+  d3.csv("../data/usHeatExtremes.csv", d => {
+    if(d.Change_in_95_percent_Days != 0)
+       return d
+  }),
+]).then(([geojson, state]) => {
   
-  // SPECIFY PROJECTION
- 
-
-  // DEFINE PATH FUNCTION
-
-
-  // APPEND GEOJSON PATH  
+  // SPECIFY PROJECTION: IN SCALES AREA SPECIFY PROJECTION as scale
+  const projection = d3.geoAlbersUsa()
+     .fitSize([width - margin.left - margin.right, 
+      height - margin.top - margin.bottom], geojson)
   
+   // Scale of dots
+  const redScale = d3.scaleSequential()
+                      .domain([0,54])
+                      .interpolator(d3.interpolateReds)                     
+  const blueScale = d3.scaleSequential()
+                      .domain([-54,0])
+                      .interpolator(d3.interpolateBlues)
+
+  // CREATE SVG ELEMENT
+  const svg = d3.select("#container")
+              .append("svg")
+              .attr("width", width)
+              .attr("height", height)
   
-  // APPEND DATA AS SHAPE
+  // add chart title on the top and description on the bottom
+  svg.append("text")
+     .attr("x", width/2-margin.left-margin.right)
+     .attr("y", margin.top) 
+     .attr("font-size","20px")
+     .attr("stroke", "navy")
+     .text("<US Heat Extremes>")
+  
+  svg.append("text")
+     .attr("x", width/2)
+     .attr("y", height-margin.bottom) 
+     .attr("font-size","15px")
+     .attr("text-anchor","middle")
+     .attr("stroke", "green")     
+     .text("Places where temperature changed in 95 percent of days.")
+
+  // PREPARE TO JOIN DATA+DRAW GEO OUTLINES
+  // DEFINE PATH FUNCTION TO DRAW LINES
+  const pathGen = d3.geoPath(projection)
+
+
+  // SELECTALL-DATA-JOIN
+  // Because we joined two datasets, we need 2 joins here.
+  
+  svg.selectAll("path")
+      .data(geojson.features)
+      .join("path")
+      .attr("d", d => pathGen(d))
+      .attr("fill", "#F5EACB")
+      .attr("stroke", "black")
+
+  svg.selectAll("circle")
+      .data(state)
+      .join(
+        enter => enter
+        .append("circle")
+        .attr("r", 3)
+        .attr("transform", d => {
+            const [x, y] = projection([d.Long, d.Lat])
+            return `translate(${x}, ${y})`
+        })
+        .attr("fill","green")
+        .call(enter => enter.transition()
+              .delay(200)
+              .attr("r", 5)
+              .attr("fill", d=> {
+                  if(d.Change_in_95_percent_Days > 0)
+                    return redScale(d.Change_in_95_percent_Days)
+                  else
+                    return blueScale(d.Change_in_95_percent_Days)
+                  }
+              )
+        )
+      )
 
 });
+
+// add color label 
+const svg = d3.select("#legend")
+              .append("svg")
+              .attr("width", 150)
+              .attr("height", 20)
+
+ svg.append("rect")
+ .attr("width",20)
+ .attr("height",10)
+ .attr("x",margin.left)
+ .attr("fill", d3.interpolateRdBu(0.1))
+
+svg.append("rect")
+ .attr("width",20)
+ .attr("height",10)
+ .attr("x",margin.left+20)
+ .attr("fill", d3.interpolateRdBu(0.3))
+
+svg.append("rect")
+ .attr("width",20)
+ .attr("height",10)
+ .attr("x",margin.left+40)
+ .attr("fill", d3.interpolateRdBu(0.5))
+
+svg.append("rect")
+ .attr("width",20)
+ .attr("height",10)
+ .attr("x",margin.left+60)
+ .attr("fill", d3.interpolateRdBu(0.7))   
+
+svg.append("rect")
+ .attr("width",20)
+ .attr("height",10)
+ .attr("x",margin.left+80)
+ .attr("fill", d3.interpolateRdBu(0.9)) 
